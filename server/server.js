515b8ed470b6ad03a3b5a0888780ed3edb5c5758
DonaTT <116920667+dDonaTT@users.noreply.github.com>
2026-04-08 23:42:29 +0200

    server configured

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server running on port ${port}`))
